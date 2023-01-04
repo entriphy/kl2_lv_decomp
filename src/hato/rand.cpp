f41fedb9a97f4d5746c68ec3d67940d34b3cff0f
entriphy <1747088+entriphy@users.noreply.github.com>
2023-01-03 21:39:08 -0800

    Use CMake instead of Makefile

--- src/hato/rand.cpp
+++ src/hato/rand.cpp
@@ -8,7 +8,11 @@ void htInitRand(int seed) {
     }
 }
 
-int htGetRand() {
-    ht_g_rand = (ht_g_rand & 1U) << 7 ^ ((ht_g_rand & 1U) << 0xe | (int)(ht_g_rand & 0x7fffU) >> 1);
+int htGetRand(void) {
+    ht_g_rand &= 0x7FFF;
+    int odd = ht_g_rand & 1;
+    ht_g_rand >>= 1;
+    ht_g_rand |= odd << 14;
+    ht_g_rand ^= odd << 7;
     return ht_g_rand - 1;
 }
