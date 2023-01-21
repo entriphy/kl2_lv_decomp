fb958130e7f79d8eb4100dd5a93ea52ae835cd2e
entriphy <1747088+entriphy@users.noreply.github.com>
2023-01-21 14:37:12 -0800

    gim: Add a couple simple functions

---
+++ src/gim.cpp
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include "gim.h"
+#include "common.h"
+
+GIMWORK* GetGimWork() {
+    GIMWORK* work = (GIMWORK*)malloc(sizeof(GIMWORK));
+    memset(work, 0, sizeof(GIMWORK));
+    return work;
+}
+
+int gmGetGimType(OBJWORK* objw) {
+    GIMWORK* gim = objw->gimmick;
+    if (gim != nullptr) {
+        if (gim->type & 2 == 0) {
+            return (gim->type >> 1) & 2;
+        }
+        return 1;
+    }
+    return 0;
+}
