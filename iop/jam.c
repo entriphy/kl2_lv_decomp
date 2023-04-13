ed9f08db004f89a3665d8df7f8bc29ced15bb4f3
entriphy <1747088+entriphy@users.noreply.github.com>
2023-04-13 00:30:31 -0700

    iop: Decompile jam.c

--- iop/jam.c
+++ iop/jam.c
@@ -1,48 +1,112 @@
-// *****************************************************************************
-// FILE -- /home/hoshino/klonoa2/src/hoshino/iop/src/jam.c
-// *****************************************************************************
-
 #include "iop.h"
 
-/* data 10 */ static int transpitch[0];
-/* bss 7e0 */ JAMDATA JamData;
-/* bss c */ JAMDATA *jM;
+static int transpitch[0];
+JAMDATA JamData;
+JAMDATA *jM;
 
-/* 00003af0 00003b64 */ u_int* GetFHMAddress(/* 0x0(sp) */ u_int *pAddr, /* 0x4(sp) */ int nNum) {
+u32 * GetFHMAddress(u32 *pAddr, s32 nNum) {
+    return (pAddr[nNum + 1] >> 2) + pAddr;
+}
+
+int JamGetBdSize(int id) {
+    JAMHD *pHD;
+    CHK_HEAD *pHead;
+
+    pHD = (JAMHD *)jM->hdAddr[id];
+    pHead = &pHD->Header;
+    return pHead->bodySize;
+}
+
+void JamGetPrm(int id, int prog, int splt, SNDKEYPRM *prm) {
+    JAMHD *pHD;
+	CHK_HEAD *pHead;
+	CHK_PROG *pProg;
+	CHK_SSET *pSSet;
+	CHK_SMPL *pSmpl;
+	CHK_VINFO *pVInfo;
+	PROG_PRMBLK *pProgPrm;
+	SPLT_PRMBLK *pSpltPrm;
+	SSET_PRMBLK *pSSetPrm;
+	SMPL_PRMBLK *pSmplPrm;
+	VINFO_PRMBLK *pVInfoPrm;
+	u32 addr;
+	u32 *ptr;
+	u16 *ptr2;
+	s32 vol;
+
+    // what
+    pHD = (JAMHD *)jM->hdAddr[id];
+    pHead = &pHD->Header;
+    pProg = (CHK_PROG *)(jM->hdAddr[id] + pHead->programChunkAddr);
+    ptr = &pProg->pointer;
+    pProgPrm = (PROG_PRMBLK *)((u32)pProg + ptr[prog]);
+    pSpltPrm = (SPLT_PRMBLK *)&pProgPrm->pointer;
+    pSpltPrm = &pSpltPrm[splt];
+    pSSet = (CHK_SSET *)(jM->hdAddr[id] + pHead->samplesetChunkAddr);
+    ptr = &pSSet->pointer;
+    pSSetPrm = (SSET_PRMBLK *)((u32)pSSet + ptr[pSpltPrm->sampleSetIndex]);
+    ptr2 = &pSSetPrm->pointer;
+    pSmpl = (CHK_SMPL *)(jM->hdAddr[id] + pHead->sampleChunkAddr);
+    ptr = &pSmpl->pointer;
+    pSmplPrm = (SMPL_PRMBLK *)((u32)pSmpl + ptr[pSSetPrm->pointer]);
+    pVInfo = (CHK_VINFO *)(jM->hdAddr[id] + pHead->vagInfoChunkAddr);
+    ptr = &pVInfo->pointer;
+    pVInfoPrm = (VINFO_PRMBLK *)((u32)pVInfo + ptr[pSmplPrm->VagIndex]);
     
+    prm->VmixL = pSmplPrm->sampleSpuAttr & 1;
+    prm->VmixR = pSmplPrm->sampleSpuAttr >> 1 & 1;
+    prm->VmixEL = pSmplPrm->sampleSpuAttr >> 2 & 1;
+    prm->VmixER = pSmplPrm->sampleSpuAttr >> 3 & 1;
+    prm->Core = pSmplPrm->sampleSpuAttr >> 5 & 1;
+    addr = jM->bdAddr[id] + pVInfoPrm->vagOffsetAddr;
+    prm->SSA = addr & 0x3FFFF8;
+    prm->ADSR1 = pSmplPrm->sampleAdsr1;
+    prm->ADSR2 = pSmplPrm->sampleAdsr2;
+    
+    if (pProgPrm->progPanpot * pSpltPrm->splitPanpot * pSmplPrm->samplePanpot < 0) {
+        vol = (pProgPrm->progVolume * -0x4000 * pSpltPrm->splitVolume) / 0x4000;
+    } else {
+        vol = (pProgPrm->progVolume * 0x3FFF * pSpltPrm->splitVolume) / 0x4000;
+    }
+    vol = (vol * pSmplPrm->sampleVolume) / 0x80;
+
+    prm->VOLL = (u16)vol & 0x7FFF;
+    prm->VOLR = (u16)vol & 0x7FFF;
+    prm->PITCH = ((transpitch[pSpltPrm->splitTranspose + 0xC] * pVInfoPrm->vagSampleRate) / 48000) & 0x3FFF;
+    prm->BendRangeL = pSpltPrm->splitBendRangeLow;
+    prm->BendRangeH = pSpltPrm->splitBendRangeHigh;
+    prm->Loop = pVInfoPrm->vagAttribute;
 }
 
-/* 00003b64 00003bdc */ int JamGetBdSize(/* 0x0(sp) */ int id) {
-	/* -0x10(sp) */ JAMHD *pHD;
-	/* -0xc(sp) */ CHK_HEAD *pHead;
+void JamBankSet(int id) {
+    if (id == 0) {
+        jM->bdAddr[id] = Mem.sBankbd;
+    } else {
+        jM->bdAddr[id] = jM->bdAddr[id - 1] + JamGetBdSize(id - 1);
+    }
+
+    jM->hdAddr[id] = Mem.iBankhd + id * 0x8000;
+    jM->transAddr = jM->bdAddr[id];
+    jM->transSize = JamGetBdSize(id);
 }
 
-/* 00003bdc 000041c0 */ void JamGetPrm(/* 0x0(sp) */ int id, /* 0x4(sp) */ int prog, /* 0x8(sp) */ int splt, /* 0xc(sp) */ SNDKEYPRM *prm) {
-	/* -0x50(sp) */ JAMHD *pHD;
-	/* -0x4c(sp) */ CHK_HEAD *pHead;
-	/* -0x48(sp) */ CHK_PROG *pProg;
-	/* -0x44(sp) */ CHK_SSET *pSSet;
-	/* -0x40(sp) */ CHK_SMPL *pSmpl;
-	/* -0x3c(sp) */ CHK_VINFO *pVInfo;
-	/* -0x38(sp) */ PROG_PRMBLK *pProgPrm;
-	/* -0x34(sp) */ SPLT_PRMBLK *pSpltPrm;
-	/* -0x30(sp) */ SSET_PRMBLK *pSSetPrm;
-	/* -0x2c(sp) */ SMPL_PRMBLK *pSmplPrm;
-	/* -0x28(sp) */ VINFO_PRMBLK *pVInfoPrm;
-	/* -0x24(sp) */ u32 addr;
-	/* -0x20(sp) */ u32 *ptr;
-	/* -0x1c(sp) */ u16 *ptr2;
-	/* -0x18(sp) */ s32 vol;
+void JamBdTrans() {
+    int size;
+
+    if (jM->transSize < 0x10000) {
+        size = jM->transSize;
+    } else {
+        size = 0x10000;
+    }
+    
+    sD->DmaWait = 1;
+    sceSdVoiceTrans(0, 0, Mem.iBankhd + 0x10000, jM->transAddr, size);
+    jM->transSize -= size;
+    jM->transAddr += size;
+    while (sD->DmaWait != 0);
 }
 
-/* 000041c0 00004320 */ void JamBankSet(/* 0x0(sp) */ int id) {
-
-}
-
-/* 00004320 00004458 */ void JamBdTrans() {
-	/* -0x10(sp) */ int size;
-}
-
-/* 00004458 00004498 */ void JamInit() {
-
+void JamInit() {
+    jM = &JamData;
+    jM->BankNum = 0;
 }
