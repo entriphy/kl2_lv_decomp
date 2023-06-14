3761276fdeaa35c459d4e5860d9a4d4c26449053
entriphy <1747088+entriphy@users.noreply.github.com>
2023-06-13 23:21:18 -0700

    nakano: Fix nkGsSetNormalFZ2

--- src/nakano/gssub.c
+++ src/nakano/gssub.c
@@ -67,35 +67,37 @@ void nkGsSetNormalFZ2_1() {
 }
 
 void nkGsSetNormalFZ2() {
-    nkGifPacket pk;
-    sceGsDrawEnv2 *drawEnv;
+    nkGifPacket packet;
+    nkGifPacket *pk;
+    sceGsDrawEnv2 *env2;
 
-    if ((GameGbl.fr - 1 & 1) == 0) {
-        drawEnv = &GameGbl.db.draw02;
+    pk = &packet;
+    if ((GameGbl.fr - 1 & 1) != 0) {
+        env2 = &GameGbl.db.draw12;
     } else {
-        drawEnv = &GameGbl.db.draw12;
+        env2 = &GameGbl.db.draw02;
     }
         
-    pk.size = 0;
-    pk.buf = nkSPR;
-    pk.buf->ul128 = 0;
+    pk->size = 0;
+    pk->buf = nkSPR;
+    pk->buf->ul128 = 0;
 
-    pk.buf[pk.size++].ul32[0] = 0x70000004;
+    pk->buf[pk->size++].ul32[0] = 0x70000004;
     
-    pk.buf[pk.size].ul64[0] = SCE_GIF_SET_TAG(3, 1, 0, 0, 0, 1);
-    pk.buf[pk.size++].ul64[1] = 0xE;
+    pk->buf[pk->size].ul64[0] = SCE_GIF_SET_TAG(3, 1, 0, 0, 0, 1);
+    pk->buf[pk->size++].ul64[1] = 0xE;
 
-    pk.buf[pk.size].ul64[1] = SCE_GS_FRAME_2;
-    pk.buf[pk.size++].ul64[0] = *(u64 *)&drawEnv->frame2;
+    pk->buf[pk->size].ul64[1] = SCE_GS_FRAME_2;
+    pk->buf[pk->size++].ul64[0] = *(u64 *)&env2->frame2;
 
-    pk.buf[pk.size].ul64[1] = SCE_GS_XYOFFSET_2;
-    pk.buf[pk.size++].ul64[0] = *(u64 *)&drawEnv->xyoffset2;
+    pk->buf[pk->size].ul64[1] = SCE_GS_XYOFFSET_2;
+    pk->buf[pk->size++].ul64[0] = *(u64 *)&env2->xyoffset2;
 
-    pk.buf[pk.size].ul64[1] = SCE_GS_ZBUF_2;
-    pk.buf[pk.size++].ul64[0] = *(u64 *)&drawEnv->zbuf2;
+    pk->buf[pk->size].ul64[1] = SCE_GS_ZBUF_2;
+    pk->buf[pk->size++].ul64[0] = *(u64 *)&env2->zbuf2;
 
     DmaChGIF->chcr.TTE = 1;
-    sceDmaSend(DmaChGIF, (void *)((u32)pk.buf | 0x80000000));
+    sceDmaSend(DmaChGIF, (void *)((u32)pk->buf | 0x80000000));
     sceDmaSync(DmaChGIF, 0, 0);
 }
 
