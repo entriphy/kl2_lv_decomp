9d5ed96e71966e6b6be15cc35b6f9eb18c02be70
entriphy <1747088+entriphy@users.noreply.github.com>
2023-06-25 17:29:50 -0700

    hoshino: Decompile h_wkmng.c

--- include/hoshino.h
+++ include/hoshino.h
@@ -979,8 +979,11 @@ extern u32* GetFHMAddress(u32 *pAddr, u32 nNum);
 extern s32  hFileSize(char *filename);
 extern s32  hFileLoad(char *filename,  s32 buff);
 extern s32  hFileRead(char *filename, s32 buff, s32 *size);
-extern u32  GetFHMNum(u32 *pAddr);
-extern u32* GetFHMAddress(u32 *pAddr, u32 nNum);
+
+// h_wkmng.c
+extern void hMngInit(hMNGLIST *pMng, s32 max, s32 id);
+extern s32  hWorkGet(s32 id);
+extern void hWorkDel(s32 id, s32 idx);
 
 #pragma endregion Functions
 
---
+++ src/hoshino/h_wkmng.c
@@ -0,0 +1,39 @@
+#include "common.h"
+
+static hMNG Mng[4];
+
+void hMngInit(hMNGLIST *pMng, s32 max, s32 id) {
+    s32 i;
+
+    Mng[id].Idx = 0;
+    Mng[id].Max = max;
+    Mng[id].pList = pMng;
+    for (i = 0; i < max; i++) {
+        pMng[i].Idx = i;
+        pMng[i].Busy = 0;
+        pMng[i].Next = i + 1;
+    }
+
+    hG.light_col[0][0] = hG.light_col[0][1] = hG.light_col[0][2] = hG.light_col[0][3] = 0x80;
+    hG.light_col[3][0] = hG.light_col[3][1] = hG.light_col[3][2] = hG.light_col[3][3] = 0x80;
+}
+
+s32 hWorkGet(s32 id) {
+    hMNGLIST *p;
+    s32 idx;
+
+    p = &Mng[id].pList[Mng[id].Idx];
+    idx = Mng[id].Idx;
+    p->Busy = 1;
+    Mng[id].Idx = p->Next;
+    return idx;
+}
+
+void hWorkDel(s32 id, s32 idx) {
+    hMNGLIST *p;
+
+    p = &Mng[id].pList[idx];
+    p->Busy = 0;
+    p->Next = Mng[id].Idx;
+    Mng[id].Idx = idx;
+}
