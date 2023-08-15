174b836fa6b0907e26421c7eb535758cf319613c
entriphy <1747088+entriphy@users.noreply.github.com>
2023-08-15 00:04:10 -0700

    diff: Use relocation addresses from decomp ELF

--- tools/elf_diff.py
+++ tools/elf_diff.py
@@ -1,4 +1,4 @@
-from elftools.elf.elffile import ELFFile, SymbolTableSection
+from elftools.elf.elffile import ELFFile, SymbolTableSection, RelocationSection
 from colorama import Fore, Back, Style
 import argparse
 import json
@@ -23,12 +23,14 @@ class GenericElf:
     text: bytes
     text_offset: int
     functions: dict[str, Function]
+    relocation_addrs: list[int]
 
     def __init__(self, elf: ELFFile):
         self.elf = elf
         text_section = next(t for t in elf.iter_sections() if t.name == ".text")
         self.text = text_section.data()
         self.text_offset = text_section.header.sh_addr
+        self.relocation_addrs = []
 
     def get_function_instructions(self, name: str, size: int = -1) -> list[rabbitizer.Instruction]:
         func = self.functions.get(name)
@@ -54,11 +56,13 @@ class ElfWithSymbols(GenericElf):
     def __init__(self, elf: ELFFile):
         super().__init__(elf)
         symbol_table = next(s for s in elf.iter_sections() if isinstance(s, SymbolTableSection))
+        relocations = next(s for s in elf.iter_sections() if isinstance(s, RelocationSection))
         function_symbols = list(sorted(
             [sym for sym in symbol_table.iter_symbols() if sym.entry.st_info.type == "STT_FUNC"],
             key=lambda sym: sym.entry.st_value  # Sort by address
         ))
         self.functions = {func.name: Function(func.name, func.entry.st_value, func.entry.st_size) for func in function_symbols}
+        self.relocation_addrs = [r.entry.r_offset for r in relocations.iter_relocations()]
 
 
 class ElfWithJson(GenericElf):
@@ -67,17 +71,41 @@ class ElfWithJson(GenericElf):
         self.functions = {func["name"]: Function(func["name"], func["address"], -1) for func in funcs_json.get("functions")}
 
 
-def compare_functions(x: list[rabbitizer.Instruction], y: list[rabbitizer.Instruction]) -> int | None:
-    for i, instr in enumerate(x):
-        if not instr.sameOpcode(y[i]):
-            return i
-        if instr.sameOpcodeButDifferentArguments(y[i]):
-            if instr.isJump() or instr.canBeLo() or instr.canBeHi():
+class NonMatchingFunction:
+    orig_func: Function
+    decomp_func: Function
+    orig_func_instrs: list[rabbitizer.Instruction]
+    decomp_func_instrs: list[rabbitizer.Instruction]
+    idx: int
+
+    def __init__(self, orig_func, decomp_func, orig_func_instrs, decomp_func_instrs):
+        self.orig_func = orig_func
+        self.decomp_func = decomp_func
+        self.orig_func_instrs = orig_func_instrs
+        self.decomp_func_instrs = decomp_func_instrs
+        self.idx = -1
+
+
+def compare_functions(orig_elf: GenericElf, decomp_elf: GenericElf, name: str) -> NonMatchingFunction | None:
+    orig_func = orig_elf.functions[name]
+    decomp_func = decomp_elf.functions[name]
+    func_size = decomp_func.size
+    orig_func_instrs = orig_elf.get_function_instructions(name, func_size)
+    decomp_func_instrs = decomp_elf.get_function_instructions(name)
+
+    non_matching = NonMatchingFunction(orig_func, decomp_func, orig_func_instrs, decomp_func_instrs)
+    for i, instr in enumerate(orig_func_instrs):
+        if not instr.sameOpcode(decomp_func_instrs[i]):
+            non_matching.idx = i
+            break
+        if instr.sameOpcodeButDifferentArguments(decomp_func_instrs[i]):
+            if instr.isJump():
                 continue
-            if (instr.doesLoad() or instr.doesStore()) and instr.rs == rabbitizer.RegGprO32.gp:
+            if orig_func.address + i * 4 in orig_elf.relocation_addrs or decomp_func.address + i * 4 in decomp_elf.relocation_addrs:
                 continue
-            return i
-    return None
+            non_matching.idx = i
+            break
+    return None if non_matching.idx == -1 else non_matching
 
 
 def read_elf(elf_path: str, json_path: str = None) -> GenericElf:
@@ -100,16 +128,13 @@ def diff(orig_elf_path, decomp_elf_path, orig_json):
     for func_name in orig_elf.functions.keys():
         if func_name not in decomp_elf.functions:
             continue
-        func_size = decomp_elf.functions[func_name].size
-        orig_func = orig_elf.get_function_instructions(func_name, func_size)
-        decomp_func = decomp_elf.get_function_instructions(func_name)
-        result = compare_functions(orig_func, decomp_func)
+        result = compare_functions(orig_elf, decomp_elf, func_name)
         if result is not None:
-            orig_address = f"0x{orig_elf.functions[func_name].address + result * 4:08x}"
-            decomp_address = f"0x{decomp_elf.functions[func_name].address + result * 4:08x}"
+            orig_address = f"0x{result.orig_func.address + result.idx * 4:08x}"
+            decomp_address = f"0x{decomp_elf.functions[func_name].address + result.idx * 4:08x}"
             print(Fore.RED + f"\t{func_name}: FAIL" + Style.RESET_ALL)
-            print(f"\t\t{orig_address}: {orig_func[result]}")
-            print(f"\t\t{decomp_address}: {decomp_func[result]}")
+            print(f"\t\t{orig_address}: {result.orig_func_instrs[result.idx]}")
+            print(f"\t\t{decomp_address}: {result.decomp_func_instrs[result.idx]}")
         else:
             print(Fore.GREEN + f"\t{func_name}: PASS" + Style.RESET_ALL)
 
