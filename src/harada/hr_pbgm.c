b9a61efc13ce5ae67a451324a3c70a9c6a712b86
entriphy <1747088+entriphy@users.noreply.github.com>
2023-08-21 00:45:35 -0700

    harada: Decompile hr_pbgm.c

--- src/harada/hr_pbgm.c
+++ src/harada/hr_pbgm.c
@@ -1,7 +1,64 @@
-#include "hr_pbgm.h"
+#include "harada/hr_pbgm.h"
+#include "hoshino/h_str.h"
+#include "harada/hr_pall.h"
 
-// static PTBGM ptbgm_table[82] = {};
-// static PTBGM *ptb;
-PT hrpt_id = 0;
-s32 hrptm_res = 0;
+static PTBGM ptbgm_table[82] = {}; // TODO
+static PTBGM *ptb;
+s32 hrpt_id = 0;
+s16 hrptm_res = 0;
 s32 hrpt_sbgm = 0;
+
+s32 hr_ptchk_45(s32 id) {
+    s32 var;
+
+    var = id & 2 ? HRPSFG_CHK040B : HRPSFG_CHK0505 ;
+    return pt_flagj_P(var);
+}
+
+void hr_pt_setBGM(HR_PSYS *ps, s16 bgm, s32 mode) {
+    ps->reqbgm = bgm;
+    if (mode == 0) {
+        ps->flag |= 0x20000000;
+    } else {
+        ps->flag |= 0x10000000;
+    }
+}
+
+void hr_pt_reqBGM(HR_PSYS *ps) {
+    s32 mode;
+
+    mode = ps->flag & 0x20000000 ? 0 : 2;
+    ps->flag &= -0x30000001;
+    hBgmChangeID(ps->reqbgm, mode);
+}
+
+void hr_ptbgm_init(HR_PSYS *ps) {
+    s32 id;
+
+    ps->reqbgm = -1;
+    if (hrpt_id != -1 && hrpt_flag != 4) {
+        ptb = &ptbgm_table[hrpt_id];
+        if (ptb->flag & 1) {
+            hr_pt_setBGM(ps, ptb->sbgm, 0);
+        }
+        return;
+    }
+}
+
+void hr_ptbgm_end(HR_PSYS *ps, s32 mode) {
+    s32 id;
+
+    if (hrpt_id == -1 || hrpt_flag == 4) {
+        return;
+    }
+
+    ptb = &ptbgm_table[hrpt_id];
+    if (ptb->flag & 0xC) {
+        id = ptb->flag & 8;
+        hBgmChangeID(-1, id ? 0 : 2);
+    } else if (ptb->flag & 0x10) {
+        if (ps->reqbgm == -1 || ps->reqbgm != ptb->ebgm) {
+            hBgmChangeID(ptb->ebgm, mode);
+        }
+    }
+}
--- src/harada/hr_pbgm.h
+++ src/harada/hr_pbgm.h
@@ -3,8 +3,31 @@
 
 #include "harada.h"
 
-extern PT hrpt_id;
-extern s32 hrptm_res;
+typedef struct { // 0x8
+    /* 0x0 */ s16 sbgm;
+    /* 0x2 */ s16 ebgm;
+    /* 0x4 */ s16 flag;
+    /* 0x6 */ s16 pad0;
+} PTBGM;
+
+typedef enum  {
+    HRPSFG_0706IKA,
+    HRPSFG_0609IKA,
+    HRPSFG_CHK040B,
+    HRPSFG_CHK0505,
+    HRPSFG_CHKTREC,
+    HRPSFG_CHKTRG,
+    HRPSFG_PNUM
+} HRPSFG;
+
+extern s32 hrpt_id;
+extern s16 hrptm_res;
 extern s32 hrpt_sbgm;
+
+extern s32 hr_ptchk_45(s32 id);
+extern void hr_pt_setBGM(HR_PSYS *ps, s16 bgm, s32 mode);
+extern void hr_pt_reqBGM(HR_PSYS *ps);
+extern void hr_ptbgm_init(HR_PSYS *ps);
+extern void hr_ptbgm_end(HR_PSYS *ps, s32 mode);
 
 #endif
