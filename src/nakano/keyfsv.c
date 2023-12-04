c72ca41eaff4a3584cba182c37cea7418a604c5d
entriphy <1747088+entriphy@users.noreply.github.com>
2023-12-04 04:10:59 -0800

    nakano: Decompile keyfsv.c

--- src/nakano/keyfsv.c
+++ src/nakano/keyfsv.c
@@ -1,12 +1,33 @@
-#include "keyfsv.h"
+#include "nakano/keyfsv.h"
 
-// static nkKEY_DATA *key_data_top;
+static nkKEY_DATA *key_data_top;
 // static s32 key_data_top_index; // ?
 static s32 key_frame_cnt;
 static s32 key_frame_max;
 static s32 kf_mode;
 
+void nkKeyFrameSet(u32 *adr) {
+    key_frame_max = *adr;
+    adr += 2;
+    key_data_top = (nkKEY_DATA *)adr;
+    kf_mode = 0;
+}
+
 void nkKeyFrameStart() {
     key_frame_cnt = 0;
     kf_mode = 1;
+}
+
+void nkKeyFramePlay() {
+    /* a0 4 */ HERO_WORK *herow;
+    /* v1 3 */ nkKEY_DATA *now_keyf;
+
+    herow = GameGbl.klonoa->work;
+    if (key_frame_cnt < key_frame_max) {
+        now_keyf = &key_data_top[key_frame_cnt];
+        herow->gimm_flag |= 0x1000;
+        herow->ex_lvl = now_keyf->lvl;
+        herow->ex_trg = now_keyf->trg;
+        key_frame_cnt++;
+    }
 }
--- src/nakano/keyfsv.h
+++ src/nakano/keyfsv.h
@@ -3,6 +3,11 @@
 
 #include "nakano.h"
 
+typedef struct { // 0x8
+    /* 0x0 */ u32 lvl;
+    /* 0x4 */ u32 trg;
+} nkKEY_DATA;
+
 extern void nkKeyFrameStart();
 
 #endif
