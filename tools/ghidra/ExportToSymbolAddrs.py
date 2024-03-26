8bf166bd8c1816d2b1ecfa614637f9fcb7fb8dc0
entriphy <1747088+entriphy@users.noreply.github.com>
2024-03-26 13:30:10 -0700

    tools: Export all function names for symbol_addrs.txt

--- tools/ghidra/ExportToSymbolAddrs.py
+++ tools/ghidra/ExportToSymbolAddrs.py
@@ -1,18 +1,23 @@
 # Exports all symbols to a symbol_addrs.txt file
 import os
 
+all_funcs = askYesNo("meow", "Export unnamed functions?")
 directory = askDirectory("Select output directory", "Ok")
 program = getCurrentProgram()
 listing = program.getListing()
 symbolTable = program.getSymbolTable()
 
-with open(os.path.join(directory.absolutePath, "symbol_addrs.txt"), "w") as f:
+with open(os.path.join(directory.absolutePath, "symbol_addrs.txt" if not all_funcs else "symbol_addrs_all.txt"), "w") as f:
     for symbol in list(symbolTable.getAllSymbols(False)):
         address = symbol.address.unsignedOffset
         if symbol.symbolType.equals(symbol.symbolType.FUNCTION):
             if symbol.name.startswith("FUN"):
-                continue
-            name = symbol.getName(address >= 0x0023A3E8)
+                if all_funcs:
+                    name = "func_%08X" % address
+                else:
+                    continue
+            else:
+                name = symbol.getName(address >= 0x0023A3E8)
             f.write("%s = 0x%08X; // type:func\n" % (name, address))
         elif symbol.symbolType.equals(symbol.symbolType.LABEL):
             name = symbol.getName(False)
